6da1317d0bb69f3d9d60c4999d4f731926c717ff
IsraelAlan10 <alanmontero54@aragon.unam.mx>
2024-04-16 20:44:07 -0600

    latest

---
+++ Assets/Samples/Google Cardboard XR Plugin for Unity/1.23.0/Hello Cardboard/Scripts/audio.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class audio : MonoBehaviour
+{
+
+	public AudioSource reproductor;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+	    // reproductor = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+	    
+    }
+}
+
